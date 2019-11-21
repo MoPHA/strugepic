3e7886ee54d90739f6cca3e83a7dcce29383f657
Nortamo <40563680+Nortamo@users.noreply.github.com>
2019-11-21 12:18:51 +0200

    Better templating

--- internal_interpolation.hpp
+++ internal_interpolation.hpp
@@ -3,6 +3,22 @@
 namespace strugepicInternal 
 {
     
+    // Function to evaluate  piecewise compact polynominal over -2,2 consisting of 4 parts  
+    template <class T,int Pdeg>
+    T P4_eval(T x,const T* coeffs){
+        if( x >= 2 ||  x <= -2){
+            return 0;
+        }
+        else{ 
+            int start_idx = ( (int )(x+2))*(Pdeg+1);
+            T sum =coeffs[start_idx];
+            for(int i=1; i<(Pdeg+1);i++){
+                sum = coeffs[start_idx +i]+x*sum;
+            }
+            return sum;
+        }
+    }
+
     template <class T>
     T W1_8 ( T x ){
         static const T coeffs[36]={
@@ -11,22 +27,8 @@ namespace strugepicInternal
         -15.0/1024,-15.0/128,7.0/16,-21.0/32,175.0/256,0,-105.0/128,0,337.0/512, // 0 -> 1
         15.0/1024,-15.0/128,49.0/128,-21.0/32,35.0/64,0,0,-1,1 //1->2
         };
-
-        
-        if( x >= 2 ||  x <= -2){
-            return 0;
-        }
-        else{ 
-            int start_idx = ( (int )(x+2))*9;
-            T sum =coeffs[start_idx];
-            for(int i=1; i<9;i++){
-                sum = coeffs[start_idx +i]+x*sum;
-            }
-            return sum;
-        }
-    
+        return P4_eval<T,8>(x,coeffs);    
     }
-
 
 
     
@@ -39,19 +41,7 @@ namespace strugepicInternal
          -15.0/128,-105.0/128,21.0/8,-105.0/32,175.0/64,0,-105.0/64,0,  // 0  -> 1
          15.0/128, -105.0/128, 147.0/64,-105.0/32 ,35.0/16,0,0,-1 // 2 -> 1 
         };
-
-        if( q >= 2 ||  q <= -2){
-            return 0;
-        }
-        else{
-            int start_idx = ( (int )(q+2))*8;
-            T sum =coeffs[start_idx];
-            for(int i=1; i<8;i++){
-                sum = coeffs[start_idx +i]+q*sum;
-            }
-            return sum;
-        }
-
+        return P4_eval<T,7>(q,coeffs);
     }
 
     template <class T>
@@ -66,7 +56,11 @@ namespace strugepicInternal
         
     }
 
-
- 
+template <class T,int Pdeg>
+   T W(T x){
+        T (* InterPol1D [1] )(T) ={W1_8<T>};
+        // Lowest order is 8
+        return InterPol1D[Pdeg-8](x);
+    }
  
 }
--- interpolation.cpp
+++ interpolation.cpp
@@ -2,20 +2,20 @@
 #ifndef INTERPOL
 #define INTERPOL
 
-#define POLY_DEGREE W1_8
+#define POLY_DEGREE 8
 
 // This is just some basic stuff
 // Need to think about the usability
 namespace strugepic
 {    
     // Change this and recompile to have a different interpolating function
-    auto const fp_d = &strugepicInternal::POLY_DEGREE<double>;
+    auto const fp_d = &strugepicInternal::W<double,POLY_DEGREE>;
     
 __attribute__ ((visibility ("default")))   double W ( double x , double y , double z){
         return fp_d(x)*fp_d(y)*fp_d(z);
     }
 
-    auto const fp_f = &strugepicInternal::POLY_DEGREE<float>;
+    auto const fp_f = &strugepicInternal::W<float,POLY_DEGREE>;
 
 __attribute__ ((visibility ("default")))    float W (float x,float y,float z ){
         return fp_f(x)*fp_f(y)*fp_f(z);
