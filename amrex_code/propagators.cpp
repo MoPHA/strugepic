7fc071a4b0dff4a369ead838b62010c6836e310d
Nortamo <40563680+Nortamo@users.noreply.github.com>
2019-12-10 09:49:10 +0200

    Moved amrex code location

--- amrex_code/propagators.cpp
+++ amrex_code/propagators.cpp
@@ -1,4 +1,5 @@
 #include "AMReX_Box.H"
+#include "AMReX_Geometry.H"
 
 void push_B( amrex::Box const& bx,  amrex::Array4<amrex::Real> const& B, amrex::Array4<amrex::Real> const& E,double dt){
    const auto lo = amrex::lbound(bx);
@@ -31,9 +32,36 @@ void push_E( amrex::Box const& bx,  amrex::Array4<amrex::Real> const& E, amrex::
 }
 
 template<class Particles>
-void push_V_E( const Particles&particles ){
+void push_V_E( const Particles&particles, const amrex::Geometry geom,double dt,amrex::Array4<amrex::Real> const& E ){
     for(auto& p : particles){
-        p.getreal(3)=1;
+        auto index =get_point_cell(geom,{p.pos(0),p.pos(1),p.pos(2)}) ;
+        // Basis functions are supported over two cells in each direction
+        // Need some other particle strucutre to have constant m and q for some
+        const int idx_list[4]={-2,1,0,1};
+        const double m= p.rdata(0);
+        const double q= p.rdata(1);
+       
+        const double coef = dt*q/m;
+        const double inv_dx = 1/geom.CellSize(0);
+        const double inv_dy = 1/geom.CellSize(1);
+        const double inv_dz = 1/geom.CellSize(2);
+        double dvx=0;
+        double dvy=0;
+        double dvz=0;
+
+
+        for(auto k: idx_list){
+            for(auto j: idx_list){
+                for(auto i: idx_list){
+                   dvx+= 
+                   dvy+= 
+                   dvz+= 
+                }
+            }
+        }
+        p.rdata(2)+=dvx*coef;
+        p.rdata(3)+=dvy*coef;
+        p.rdata(4)+=dvz*coef;
     }
 }
 
