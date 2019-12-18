bb3288c1999df1bd84115a13c0852ee33da1eb87
Nortamo <40563680+Nortamo@users.noreply.github.com>
2019-12-18 09:50:36 +0200

    Cleaner implementation of pushers
    Removed switch case statements

--- amrex_code/propagators.hpp
+++ amrex_code/propagators.hpp
@@ -5,7 +5,7 @@
 #include "particle_defs.hpp"
 #include "amrex_util.hpp"
 
-// These are All local update functions
+// These are All local update functions, I.e they operate only on local data 
 
 void push_B( amrex::Box const& bx,  amrex::Array4<amrex::Real> const& B, amrex::Array4<amrex::Real> const& E,double dt);
 
@@ -23,7 +23,7 @@ void Theta_y(CParticleContainer&ParticleC, CParticles&local_particles,const CNPa
 
 void Theta_z(CParticleContainer&ParticleC, CParticles&local_particles,const CNParticles&neighbour_particles, const amrex::Geometry geom,amrex::Array4<amrex::Real> const& E, amrex::Array4<amrex::Real> const& B ,double dt);
 
-// Global update, they also handle triggering the updates
+// Global update, they also handle triggering global communication
 void G_Theta_E(const amrex::Geometry geom,CParticleContainer&P, amrex::MultiFab &E, amrex::MultiFab &B,double dt );
 void G_Theta_B(const amrex::Geometry geom,CParticleContainer&P, amrex::MultiFab &E, amrex::MultiFab &B,double dt );
 void G_Theta_x(const amrex::Geometry geom,CParticleContainer&P, amrex::MultiFab &E, amrex::MultiFab &B,double dt );
@@ -41,50 +41,43 @@ void G_Theta_z(const amrex::Geometry geom,CParticleContainer&P, amrex::MultiFab 
 template<int comp,class Pcontainter>
 void push_E_part(const Pcontainter&particles, const amrex::Geometry geom,amrex::Array4<amrex::Real> const& E ,double dt){ 
     const int idx_list[4]={-1,0,1,2};
-    const  double coef = particles[0].rdata(1)*geom.InvCellSize(0)*geom.InvCellSize(1)*geom.InvCellSize(2);
+    const  double coef = particles[0].rdata(Q)*geom.InvCellSize(X)*geom.InvCellSize(Y)*geom.InvCellSize(Z);
     const auto low = geom.ProbLo();
     const auto Ics = geom.InvCellSize();
 
     for(auto& p : particles){
         const auto p_segments = get_segment_list<comp>(geom, p.pos(comp) , p.pos(comp)+dt*p.rdata(comp+2));
-        auto coord =get_point_cell(geom,{p.pos(0),p.pos(1),p.pos(2)}) ;
+        auto coord =get_point_cell(geom,{p.pos(X),p.pos(Y),p.pos(Z)}) ;
+        
         for(auto seg: p_segments){
             coord[comp] = std::get<2>(seg);
             auto i_s = std::get<0>(seg); 
             auto i_e = std::get<1>(seg);
 
-    for(auto k: idx_list){
-            for(auto j: idx_list){
-                for(auto i: idx_list){
-                    amrex::Real res;
-                    auto cx = coord[0]+i;
-                    auto cy = coord[1]+j;
-                    auto cz = coord[2]+k;
-                    using namespace strugepic;
+            for(auto k: idx_list){
+                for(auto j: idx_list){
+                    for(auto i: idx_list){
+                        amrex::Real res;
+                        int cl[3];
+                        cl[X] = coord[X]+i;
+                        cl[Y] = coord[Y]+j;
+                        cl[Z] = coord[Z]+k;
+                        auto comp_u = (comp+1)%3;
+                        auto comp_l = (comp+2)%3;
+                        
+                        using namespace strugepic;
+                        if(E.contains(cl[X],cl[Y],cl[Z])){
+                            res=I_W12((i_s-low[comp])*Ics[comp]- cl[comp] ,(i_e-low[comp])*Ics[comp]- cl[comp])
+                                *W1((p.pos(comp_u)-low[comp_u])*Ics[comp_u]-cl[comp_u])
+                                *W1((p.pos(comp_l)-low[comp_l])*Ics[comp_l]-cl[comp_l]);
 
-                    if(E.contains(cx,cy,cz)){
-                    switch(comp){
-                        case 0:
-                            res=I_W12((i_s-low[0])*Ics[0]- cx ,(i_e-low[0])*Ics[0]- cx)*W1((p.pos(1)-low[1])*Ics[1]-cy  )*W1((p.pos(2)-low[2])*Ics[2]-cz );
-                            break;
-                        case 1:
-                            res=I_W12((i_s-low[1])*Ics[1]- cy ,(i_e-low[1])*Ics[1]- cy)*W1((p.pos(2)-low[2])*Ics[2]-cz )*W1( (p.pos(0)-low[0])*Ics[0]-cx   );
-                            break;
-                        case 2:
-                            res=I_W12((i_s-low[2])*Ics[2]- cz ,(i_e-low[2])*Ics[2]- cz)*W1( (p.pos(0)-low[0])*Ics[0]-cx   )*W1((p.pos(1)-low[1])*Ics[1]-cy  );
-                            break;
-                
+                            E(cl[X],cl[Y],cl[Z],comp)-=coef*p.rdata(comp+2)*res;
+                        }
                     }
-                    E(cx,cy,cz,comp)-=coef*p.rdata(comp+2)*res;
-                    }
-                    
                 }
-
             }
+        }
     }
-    }
-}
-    
 }
 
 
@@ -108,61 +101,53 @@ void push_V_p(CParticleContainer&p_container ,CParticles&local_particles,double 
 template<int comp>
 void push_B_p(CParticles&particles, const amrex::Geometry geom, const amrex::Array4<amrex::Real> & B ,double dt){ 
     const int idx_list[4]={-1,0,1,2};
-    const  double coef = particles[0].rdata(1)/particles[0].rdata(0);
+    const  double coef = particles[0].rdata(Q)/particles[0].rdata(M);
     const auto low = geom.ProbLo();
     const auto Ics = geom.InvCellSize();
 
     for(auto& p : particles){
         const auto p_segments = get_segment_list<comp>(geom, p.pos(comp) , p.pos(comp)+dt*p.rdata(comp+2));
-        auto coord =get_point_cell(geom,{p.pos(0),p.pos(1),p.pos(2)}) ;
-
+        auto coord =get_point_cell(geom,{p.pos(Z),p.pos(Y),p.pos(Z)}) ;
         amrex::Real res_c1=0;
         amrex::Real res_c2=0;
+
         for(auto seg: p_segments){
             coord[comp] = std::get<2>(seg);
             auto i_s = std::get<0>(seg); 
             auto i_e = std::get<1>(seg);
-
-
-
+            
 // The W_1^{(2)} function is supported over -1 < x< 2 so symmetric looping is not the best, but will do for now.
-    double norm_res=0;
-    for(auto k: idx_list){
-            for(auto j: idx_list){
-                for(auto i: idx_list){
-                    auto cx = coord[0]+i;
-                    auto cy = coord[1]+j;
-                    auto cz = coord[2]+k;
-                    using namespace strugepic;
- //                   norm_res+=W( (p.pos(0)-low[0])*Ics[0]-cx  , (p.pos(1)-low[1])*Ics[1]-cy    ,  (p.pos(2)-low[2])*Ics[2]-cz ) ;
-                    switch(comp){
-                        case 0:
-                            res_c1+=B(cx,cy,cz,1)*I_W12((i_s-low[0])*Ics[0]- cx ,(i_e-low[0])*Ics[0]- cx)*W1(  (p.pos(1)-low[1])*Ics[1]-cy  )*W12( (p.pos(2)-low[2])*Ics[2]-cz  );
-                            res_c2-=B(cx,cy,cz,2)*I_W12((i_s-low[0])*Ics[0]- cx ,(i_e-low[0])*Ics[0]- cx)*W12( (p.pos(1)-low[1])*Ics[1]-cy  )*W1( (p.pos(2)-low[2])*Ics[2]-cz  );
-                            break;
-                        case 1:
-                            res_c1+=B(cx,cy,cz,2)*I_W12((i_s-low[1])*Ics[1]- cy ,(i_e-low[1])*Ics[1]- cy)*W12( (p.pos(0)-low[0])*Ics[0]-cx  )*W1( (p.pos(2)-low[2])*Ics[2]-cz  );
-                            res_c2-=B(cx,cy,cz,0)*I_W12((i_s-low[1])*Ics[1]- cy ,(i_e-low[1])*Ics[1]- cy)*W1( (p.pos(0)-low[0])*Ics[0]-cx  )*W12( (p.pos(2)-low[2])*Ics[2]-cz  );
-                            break;
-                        case 2:
-                            res_c1+=B(cx,cy,cz,0)*I_W12((i_s-low[2])*Ics[2]- cz ,(i_e-low[2])*Ics[2]- cz)*W1( (p.pos(0)-low[0])*Ics[0]-cx  )*W12((p.pos(1)-low[1])*Ics[1]-cy  );
-                            res_c2-=B(cx,cy,cz,1)*I_W12((i_s-low[2])*Ics[2]- cz ,(i_e-low[2])*Ics[2]- cz)*W12( (p.pos(0)-low[0])*Ics[0]-cx  )*W1((p.pos(1)-low[1])*Ics[1]-cy);
-                            break;
-                
-                    }
+            for(auto k: idx_list){
+                for(auto j: idx_list){
+                    for(auto i: idx_list){
+                        int cl[3];
+                        cl[X] = coord[X]+i;
+                        cl[Y] = coord[Y]+j;
+                        cl[Z] = coord[Z]+k;
+                        auto comp_u = (comp+1)%3;
+                        auto comp_l = (comp+2)%3;
+                        
+                        using namespace strugepic;//norm_res+=W( (p.pos(0)-low[0])*Ics[0]-cx  , (p.pos(1)-low[1])*Ics[1]-cy    ,  (p.pos(2)-low[2])*Ics[2]-cz ) ;            
 
-                    
-                }
+                        res_c1+=B(cl[X],cl[Y],cl[Z],comp_u)
+                        *I_W12((i_s-low[comp])*Ics[comp]- cl[comp] ,(i_e-low[comp])*Ics[comp]- cl[comp])
+                        *W1(  (p.pos(comp_u)-low[comp_u])*Ics[comp_u]-cl[comp_u])
+                        *W12( (p.pos(comp_l)-low[comp_l])*Ics[comp_l]-cl[comp_l]);
+                        
+                        res_c2+=-B(cl[X],cl[Y],cl[Z],comp_l)
+                        *I_W12((i_s-low[comp])*Ics[comp]- cl[comp] ,(i_e-low[comp])*Ics[comp]- cl[comp])
+                        *W12( (p.pos(comp_u)-low[comp_u])*Ics[comp_u]-cl[comp_u])
+                        *W1(  (p.pos(comp_l)-low[comp_l])*Ics[comp_l]-cl[comp_l]);
 
-            }
-    }   
-    }
+                    } // over i
+                } // over j
+            } // over k  
+        } // over segments
+
         p.rdata( (comp +2 )% 3 +2   )+=coef*res_c1*p.rdata(comp+2); 
         p.rdata( (comp+1) % 3 +2  )+=coef*res_c2*p.rdata(comp+2); 
-}
-    
-}
-
+    } // over particles
+} // function end
 
 #endif
 
