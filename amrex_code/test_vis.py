b8e5b9cef4e414c8fb9ebf290d5906e2a94dc474
Nortamo <40563680+Nortamo@users.noreply.github.com>
2019-12-20 14:38:00 +0200

    Plotting util

--- amrex_code/test_vis.py
+++ amrex_code/test_vis.py
@@ -1,5 +1,15 @@
 import yt
-print(yt.__version__)
-ds=yt.load("plt_P00/test_particle")
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 
-print(ds.field_list)
+
+for step in range(0,700,5):
+    filename = "plt_E" + str(step)
+    ds=yt.load(filename)
+    mag=ds.r[[-1,0,0]:[1,0,0]]
+    fig, ax = plt.subplots()
+    ax.plot(mag["E_z"])
+    ax.set_ylim([-1,1])
+    plt.savefig("E_z_pic"+str(step).zfill(3)) 
+    plt.close()
