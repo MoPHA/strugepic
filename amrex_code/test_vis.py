38fcd3edb585d344661bd8c1d57774487e1980b4
Nortamo <40563680+Nortamo@users.noreply.github.com>
2019-12-23 17:59:41 +0200

    Plot total field energy

--- amrex_code/test_vis.py
+++ amrex_code/test_vis.py
@@ -26,14 +26,17 @@ X, Y = np.meshgrid(X, Y)
 #    plt.savefig("E_y_field"+str(step).zfill(3)) 
 #    plt.close()
 
-
-for step in range(0,400,5):
+val =np.zeros(100)
+i=0
+for step in range(0,100):
     B_filename = "plt_B" + str(step)
     E_filename = "plt_E" +str(step)
     dsB=yt.load(B_filename)
     dsE=yt.load(E_filename)
-    B=dsB.r[[-1,0,0]:[1,0,0]]
-    E=dsE.r[[-1,0,0]:[1,0,0]]
+    #B=dsB.r[[-1,0,0]:[1,0,0]]
+    #E=dsE.r[[-1,0,0]:[1,0,0]]
+    B=dsB.r[:,:,:]
+    E=dsE.r[:,:,:]
     Bx = B["B_x"]
     By = B["B_y"]
     Bz = B["B_z"]
@@ -42,12 +45,27 @@ for step in range(0,400,5):
     Ez = E["E_z"]
 
 
-    fig, ax = plt.subplots()
-    ax.plot(Bx)
-    ax.plot(By)
-    ax.plot(Bz)
-    ax.set_ylim([-1,1])
-    #val =np.sqrt(np.sum(np.square(mag["B_y"])))
-    #ax.set_title()
-    plt.savefig("B_pic"+str(step).zfill(3)) 
-    plt.close()
+    val[i] =np.sqrt(np.sum(np.square(Bx)  +np.square(By)+np.square(Bz)+np.square(Ex)  +np.square(Ey)+np.square(Ez)  ))
+    i=i+1
+ #   fig, ax = plt.subplots()
+ #   ax.plot(Bx)
+ #   ax.plot(By)
+ #   ax.plot(Bz)
+ #   ax.set_ylim([-1,1])
+ #   ax.set_title(str(val[i-1]))
+ #   plt.savefig("B_pic"+str(step).zfill(3)) 
+ #   plt.close()
+ #   fig, ax = plt.subplots()
+ #   ax.plot(Ex)
+ #   ax.plot(Ey)
+ #   ax.plot(Ez)
+ #   ax.set_ylim([-1,1])
+ #   ax.set_title(str(val[i-1]))
+ #   plt.savefig("E_pic"+str(step).zfill(3)) 
+ #   plt.close()
+
+
+fig, ax = plt.subplots()
+ax.plot(val[0:]/val[1])
+plt.savefig("E_tot") 
+plt.close()
