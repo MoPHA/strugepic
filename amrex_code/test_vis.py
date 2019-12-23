40ba3575d22dbfd193d7c3336bf8383b330ca2da
Nortamo <40563680+Nortamo@users.noreply.github.com>
2019-12-23 14:37:09 +0200

    more visualisation

--- amrex_code/test_vis.py
+++ amrex_code/test_vis.py
@@ -1,15 +1,53 @@
 import yt
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib import cm
+
+X = np.linspace(-1,1, 256)
+Y = np.linspace(-1,1, 256)
+X, Y = np.meshgrid(X, Y)
+
+#for step in range(0,70):
+#    filename = "plt_E"+str(step)
+#    ds=yt.load(filename)
+#    mag=ds.r[:,:,0] 
+#    arr =mag["E_y"].reshape(256,256)
+#    #fig = plt.figure()
+#    #ax = fig.gca(projection='3d')
+#
+#    plt.imshow(arr, cmap='summer', interpolation='nearest')
+#    #ax.plot_surface(X,Y,arr,cmap=cm.coolwarm)
+#    #ax.set_ylim([-1,1])
+#    #ax.set_xlim([-1,1])
+#
+#    print(step)
+#    plt.savefig("E_y_field"+str(step).zfill(3)) 
+#    plt.close()
 
 
-for step in range(0,700,5):
-    filename = "plt_B" + str(step)
-    ds=yt.load(filename)
-    mag=ds.r[[-1,0,0]:[1,0,0]]
+for step in range(0,400,5):
+    B_filename = "plt_B" + str(step)
+    E_filename = "plt_E" +str(step)
+    dsB=yt.load(B_filename)
+    dsE=yt.load(E_filename)
+    B=dsB.r[[-1,0,0]:[1,0,0]]
+    E=dsE.r[[-1,0,0]:[1,0,0]]
+    Bx = B["B_x"]
+    By = B["B_y"]
+    Bz = B["B_z"]
+    Ex = E["E_x"]
+    Ey = E["E_y"]
+    Ez = E["E_z"]
+
+
     fig, ax = plt.subplots()
-    ax.plot(mag["B_y"])
+    ax.plot(Bx)
+    ax.plot(By)
+    ax.plot(Bz)
     ax.set_ylim([-1,1])
-    plt.savefig("B_y_pic"+str(step).zfill(3)) 
+    #val =np.sqrt(np.sum(np.square(mag["B_y"])))
+    #ax.set_title()
+    plt.savefig("B_pic"+str(step).zfill(3)) 
     plt.close()
