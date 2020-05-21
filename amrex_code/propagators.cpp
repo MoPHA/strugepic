8d25a133ca5fafc06daadae2497851d22b2f8ed0
Nortamo <40563680+Nortamo@users.noreply.github.com>
2020-05-21 14:29:30 +0300

    Another partial fix

--- amrex_code/propagators.cpp
+++ amrex_code/propagators.cpp
@@ -7,6 +7,74 @@
 #include "amrex_util.hpp"
 
 
+void fill_extra_halo(amrex::Geometry geom, amrex::Array4<amrex::Real> const& A,amrex::Box bx,int ng){
+
+    for(auto coord: {X,Y,Z}){
+        auto cooord_u = (coord+1)%3;
+        auto cooord_l = (coord+2)%3;
+        const auto domainLo = geom.Domain().loVect();
+        const auto subgridLo =bx.loVect();
+        const auto domainHi = geom.Domain().hiVect();
+        const auto subgridHi = bx.hiVect();
+        auto missing_L =  ng-(domainHi[coord]-domainLo[coord]+1);
+        if(domainLo[coord] == subgridLo[coord] && domainHi[coord]==subgridHi[coord] && geom.isPeriodic(coord)
+                && missing_L > 0){
+                const auto e_low=bx.loVect();
+                const auto e_high=bx.hiVect();
+                auto elx=e_high[X]-e_low[X] +1;
+                auto ely=e_high[Y]-e_low[Y] +1;
+                auto elz=e_high[Z]-e_low[Z] +1;
+        
+             for(int u=0; u<=(subgridHi[cooord_u]-subgridLo[cooord_u]);u++){
+                for(int l=0; l<=(subgridHi[cooord_l]-subgridLo[cooord_l]);l++){
+                   for(int c =0; c < missing_L ; c++){
+                        
+                        std::array<int,3> ext_coord;
+                        ext_coord[cooord_u] = u+subgridLo[cooord_u];
+                        ext_coord[cooord_l] = l+subgridLo[cooord_l];
+                        ext_coord[coord] = c+subgridHi[coord]+1+(subgridHi[coord]-subgridLo[coord]+1);
+                    std::array<int,3> int_coord={
+                            (ext_coord[cooord_u]),                           
+                            (ext_coord[cooord_l]),                            
+                            ext_coord[coord]
+                    };                          
+                    int_coord[X]=mod((int_coord[X]-e_low[X]), elx)+e_low[X];
+                    int_coord[Y]=mod((int_coord[Y]-e_low[Y]), ely)+e_low[Y];
+                    int_coord[Z]=mod((int_coord[Z]-e_low[Z]), elz)+e_low[Z];
+                A(ext_coord[X],ext_coord[Y],ext_coord[Z],X)=A(int_coord[X],int_coord[Y],int_coord[Z],X);
+                A(ext_coord[X],ext_coord[Y],ext_coord[Z],Y)=A(int_coord[X],int_coord[Y],int_coord[Z],Y);
+                A(ext_coord[X],ext_coord[Y],ext_coord[Z],Z)=A(int_coord[X],int_coord[Y],int_coord[Z],Z);
+                   }
+                }
+             }
+             for(int u=0; u<=(subgridHi[cooord_u]-subgridLo[cooord_u]);u++){
+                for(int l=0; l<=(subgridHi[cooord_l]-subgridLo[cooord_l]);l++){
+                   for(int c =0; c < missing_L ; c++){
+                        
+                        std::array<int,3> ext_coord;
+                        ext_coord[cooord_u] = u+subgridLo[cooord_u];
+                        ext_coord[cooord_l] = l+subgridLo[cooord_l];
+                        ext_coord[coord] = c+subgridLo[coord]-ng;
+                    std::array<int,3> int_coord={
+                            (ext_coord[cooord_u]),                           
+                            (ext_coord[cooord_l]),                            
+                            ext_coord[coord]
+                    };                          
+                    int_coord[X]=mod((int_coord[X]-e_low[X]), elx)+e_low[X];
+                    int_coord[Y]=mod((int_coord[Y]-e_low[Y]), ely)+e_low[Y];
+                    int_coord[Z]=mod((int_coord[Z]-e_low[Z]), elz)+e_low[Z];
+                A(ext_coord[X],ext_coord[Y],ext_coord[Z],X)=A(int_coord[X],int_coord[Y],int_coord[Z],X);
+                A(ext_coord[X],ext_coord[Y],ext_coord[Z],Y)=A(int_coord[X],int_coord[Y],int_coord[Z],Y);
+                A(ext_coord[X],ext_coord[Y],ext_coord[Z],Z)=A(int_coord[X],int_coord[Y],int_coord[Z],Z);
+                   }
+                }
+             }
+
+        }
+    }    
+}
+
+
 // Soft sine plane wave source in x-direction
 E_source::E_source(amrex::Geometry geom, amrex::MultiFab &E,int pos,int comp,double E0 ,double omega ,double dt) : 
      geom(geom),
@@ -170,7 +238,7 @@ void G_Theta_E(const amrex::Geometry geom,CParticleContainer&P, amrex::MultiFab&
         amrex::Array4<amrex::Real const> const& E_loc = E.const_array(mfi);
         amrex::Array4<amrex::Real> const& B_loc = B.array(mfi); 
         push_B_E(geom,box, B_loc,E_loc,dt);
-
+        fill_extra_halo(geom,B_loc,box,B.nGrow());
     }
 
     B.FillBoundary(geom.periodicity());
@@ -186,6 +254,7 @@ void G_Theta_E(const amrex::Geometry geom,CParticleContainer&P, amrex::MultiFab&
         auto const& E_loc = E.array(mfi);
         auto const& B_loc = B.const_array(mfi);  
         push_E_B(geom,box,E_loc,B_loc,dt);
+        fill_extra_halo(geom,E_loc,box,B.nGrow());
     }
 
     E.FillBoundary(geom.periodicity());
